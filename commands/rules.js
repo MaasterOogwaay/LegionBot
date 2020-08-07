362a9556fccc49ca72f480ce969fcedbe82234c0
MurdocNiccals1 <dylanthomas1108@gmail.com>
2020-08-07 12:56:20 +0100

    Formatted the code

--- commands/rules.js
+++ commands/rules.js
@@ -1,20 +1,17 @@
 module.exports = {
-    name: 'rules',
-    description: 'List all of the servers rules.',
-    guildOnly: true,
-	execute(message, args, rulesEmbed) {
-        rulesEmbed
-            .setColor('#0099ff')
-            .setTitle('Server Rules')
-            .setDescription('Here are the rules in this server')
-            .setThumbnail('https://i.imgur.com/ViRCIyJ.png')
-            .addFields(
-                { name: 'Im a sneaky snake', value: 'blah blah\n blah bluy' },
-            )
-            .setTimestamp('')
-            .setFooter('');
-        
-        message.channel.send(rulesEmbed);
+  name: "rules",
+  description: "List all of the servers rules.",
+  guildOnly: true,
+  execute(message, args, rulesEmbed) {
+    rulesEmbed
+      .setColor("#0099ff")
+      .setTitle("Server Rules")
+      .setDescription("Here are the rules in this server")
+      .setThumbnail("https://i.imgur.com/ViRCIyJ.png")
+      .addFields({ name: "Im a sneaky snake", value: "blah blah\n blah bluy" })
+      .setTimestamp("")
+      .setFooter("");
 
-	},
+    message.channel.send(rulesEmbed);
+  },
 };
