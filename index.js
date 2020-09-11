5546083dc8cf4ccf376809542716290007db1ae7
MurdocNiccals1 <dylanthomas1108@gmail.com>
2020-09-12 00:29:34 +0100

    added the urban dictionary api

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+const querystring = require("querystring");
+const fetch = require("node-fetch");
+
+const trim = (str, max) =>
+  str.length > max ? `${str.slice(0, max - 3)}...` : str;
 
 // Import the discord.js module
 const Discord = require("discord.js");
@@ -193,6 +198,47 @@ client.on("message", (message) => {
             .catch(() => console.error("One of the emojis failed to react."));
         }
       }
+    }
+  });
+
+  client.on("message", async (message) => {
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === "urban") {
+      if (!args.length) {
+        return message.channel.send("You need to supply a search term!");
+      }
+
+      const query = querystring.stringify({ term: args.join(" ") });
+
+      const { list } = await fetch(
+        `https://api.urbandictionary.com/v0/define?${query}`
+      ).then((response) => response.json());
+
+      if (!list.length) {
+        return message.channel.send(
+          `No results found for **${args.join(" ")}**.`
+        );
+      }
+
+      const [answer] = list;
+
+      const urban = new Discord.MessageEmbed()
+        .setColor("#EFFF00")
+        .setTitle(answer.word)
+        .setURL(answer.permalink)
+        .addFields(
+          { name: "Definition", value: trim(answer.definition, 1024) },
+          { name: "Example", value: trim(answer.example, 1024) },
+          {
+            name: "Rating",
+            value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.`,
+          }
+        );
+      message.channel.send(urban);
     }
   });
 });
